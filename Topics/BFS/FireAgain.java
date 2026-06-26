7d95a5a5776a49dc29870ce2784b33c4c254afbb
Arjun Tandon <tandonarjun695@gmail.com>
2026-06-26 20:26:27 +0530

    Fireagain div2 quesC

--- Topics/BFS/FireAgain.java
+++ Topics/BFS/FireAgain.java
@@ -1,10 +1,13 @@
 package Topics.BFS;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FireAgain {
     
     public static void main(String[] args) throws IOException{
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer
     }
 }
